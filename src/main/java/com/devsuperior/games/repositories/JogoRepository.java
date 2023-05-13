32fee718870d11e6a491f5ebb4a9d5f491d0d7ef
Axel <axelddlima@hotmail.com>
2023-05-13 17:43:58 -0300

    Finalizado versao de homologação

--- src/main/java/com/devsuperior/games/repositories/JogoRepository.java
+++ src/main/java/com/devsuperior/games/repositories/JogoRepository.java
@@ -11,11 +11,11 @@ import com.devsuperior.games.projections.JogoMinProjection;
 //Classe de acesso a Dados, sem precisar montar consulta no banco de dados! legal né?
 public interface JogoRepository extends JpaRepository<Jogo, Long> {		
 	@Query(nativeQuery = true, value = """
-			SELECT jogo.id, jogo.titulo, jogo.GENERO AS `GENERO`, jogo.img_url AS imgUrl,
+			SELECT jogo.id, jogo.titulo, jogo.GENERO AS GENERO, jogo.img_url AS imgUrl,
             jogo.DESCRICAO_CURTA AS descricaoCurta, JOGO.SCORE AS SCORE, posicao.posicao
             FROM tb_jogo  jogo
 			INNER JOIN tb_posicao_jogo posicao ON jogo.id = posicao.jogo_id
-			WHERE posicao.lista_jogos_id = 1
+			WHERE posicao.lista_jogos_id = :listId
             ORDER BY posicao.posicao; 
 				""")
 	List<JogoMinProjection> searchByList(Long listId);
